c342dc990e8c0d24cbf8b570ec3cd396ef746920
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-29 00:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function volume_sphere(e) {
     //Write your code here
 	e.preventDefault();
-
+    const pi=Math.PI;
 	const rad = document.getElementById("radius").value;
 	
-    const volum = (4/3) * 3.1415 * rad*rad*rad;
+    const volum = (4/3) * pi * rad*rad*rad;
    const roundedNumber = volum.toFixed(4);
 	
-	const vol=document.getElementById("volume");
+	const vol = document.getElementById("volume");
 	vol.value=roundedNumber;
 	
 } 
